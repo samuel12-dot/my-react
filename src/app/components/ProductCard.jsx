4e3f93b6b8a9aa476d9fa2596fe5af92539edf21
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-21 16:01:00 +0100

    feat: modify product card

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -5,8 +5,7 @@ import Image from "next/image";
 export default function ProductCard({ src, title, amount }) {
     return (
         <section className="flex flex-col items-center gap-2 p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-            {/*             <Image src={`/${src > 5 ? 0 : src}.jpg`} alt={title} width={300} height={500} className="cursor-pointer hover:scale-110 transition-all"/> */}
-            <Image src={`/${src > 5 ? 0 : src}.avif`} alt={title} width={300} height={500} className="cursor-pointer hover:scale-110 transition-all"/>
+            <Image src={`/${src > 5 ? 0 : 0}.jpg`} alt={title} width={300} height={500} className="cursor-pointer hover:scale-110 transition-all"/>
             <h2 className="font-semibold">{title}</h2>
             <p className="flex items-center gap-2">
                 <FaNairaSign />
